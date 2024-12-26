4968be3e83ac28c129425ac045f312154668a617
MajdSoubh <majdsoubh53@gmail.com>
2024-12-26 16:53:49 +0300

    structure the files

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit58acd646cb87c81919d8d15d3f4904e4
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit58acd646cb87c81919d8d15d3f4904e4', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit58acd646cb87c81919d8d15d3f4904e4', 'loadClassLoader'));
