eb6855c07cb6b86b239209b61a49a85ed3838a8a
MajdSoubh <majdsoubh53@gmail.com>
2024-12-27 22:10:19 +0300

    perform CRUD operations

--- src/index.php
+++ src/index.php
@@ -1,56 +1,27 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use App\Controllers\FileController;
 use App\Services\ServiceContainer;
 use App\Providers\AppServiceProvider;
 
-interface IUser
-{
-    public function print(): void;
-}
-class User implements IUser
-{
-    static $counter = 0;
-    public function __construct(Profile $name)
-    {
-        static::$counter++;
-    }
-
-    public function print(): void
-    {
-        echo 'User ' . static::$counter . PHP_EOL;
-    }
-}
-class Profile implements IUser
-{
-    static $counter = 0;
-    public function __construct(string $name = 's')
-    {
-        static::$counter++;
-    }
-
-    public function print(): void
-    {
-        echo 'Profile ' . static::$counter . PHP_EOL;
-    }
-}
 $container = new ServiceContainer();
+
+// Register services
 AppServiceProvider::register($container);
 
-// $reflection = new ReflectionMethod(User::class, 'print');
+// Resolve the FileController
+$fileController = $container->resolve(FileController::class);
 
-// $ps = $reflection->getParameters();
-// var_dump($ps[0]->getType()->isBuiltin());
-// $u = new User(new Profile());
+$folderName = 'demo_folder';
+$filePath = "{$folderName}/example.txt";
+$content = "Hello, this is a demonstration of Service Container implementation!";
 
-// $ioc->singleton(User::class);
-$container->singleton(IUser::class, User::class);
-// var_dump(($container->resolve(User::class)));
+// Create a folder
+$fileController->createFolder($folderName);
 
+// Write to a file
+$fileController->writeToFile($filePath, $content);
 
-($container->resolve(IUser::class))->print();
-($container->resolve(IUser::class))->print();
-// ($ioc->resolve(User::class))->print();
-// var_dump(ServiceContainer::$resolving);
-// ($ioc->resolve(User::class))->print();
-// ($ioc->resolve(User::class))->print();
+// Read from the file
+$fileController->readFromFile($filePath);
